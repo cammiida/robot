988d5f42be6e2843c5437cbeb6caf63064890a01
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2016-11-14 13:18:21 +0100

    9

--- sensobs/camera_color.py
+++ sensobs/camera_color.py
@@ -1,7 +1,5 @@
 from sensobs.sensob import Sensob
 from sensor_wrappers.camera import Camera
-from imager2 import Imager
-
 
 class Camera_color_sensob(Sensob):
     def __init__(self):
@@ -12,7 +10,7 @@ class Camera_color_sensob(Sensob):
         rgbs =0
 
         for x in range(40,80):
-            rgb = image.get_pixel(x, 48)
+            rgb = image.getpixel(x, 48)
             i = rgb.index(max(rgb))
             if i == 0:
                 rgbs += 1
@@ -22,5 +20,5 @@ class Camera_color_sensob(Sensob):
 
     def update(self):
         values = super(Camera_color_sensob, self).update()
-        self.red_percentage(Imager(values[0]))
+        self.red_percentage(values[0])
         return self.red > 20
